c7972d08569986aa4f05f8b376f36549980c2c3c
Glitch (silicon-zone) <none>
2019-12-10 03:00:43 +0000

    🏩🏏 Updated with Glitch

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log('Pinging');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const { Client } = require('discord.js-commando')
+const path = require('path')
+
+
+const client = new Client({
+    commandPrefix: '.',
+    owner: '634456486046859315', // can be an array of ids like: owner: ["id", "id"]
+    invite: 'https://discord.gg/PxVmuZc',
+})
+
+
+
+
+client.registry
+.registerDefaultTypes()
+.registerGroups([
+    ['misc', 'Misc'],
+    ['moderation', 'Moderation']
+])
+.registerDefaultGroups()
+.registerDefaultCommands()
+.registerCommandsIn(path.join(__dirname, 'commands'))
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}(${client.user.id})`)
+    client.user.setActivity('WELCOME TO HALU🌛')
+})
+
+client.on('error', console.error)
+
+
+client.login('NjUzNTQxOTgwNjkyODA3Njgx.Xe7q2A.xuKDe2kgOpRmmcNBNbOjQEph6_A')
